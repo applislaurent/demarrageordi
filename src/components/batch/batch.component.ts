3b5d523a3951c138aa2ff2db48a86059dad0ccb9
applislaurent <l.perroy@wanadoo.fr>
2020-12-14 16:56:49 +0100

    ajout logiciel ou site à la demande

--- src/components/batch/batch.component.ts
+++ src/components/batch/batch.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, ViewEncapsulation, OnInit} from '@angular/core';
-import { FormsModule, FormControl, FormGroup, FormBuilder, FormArray, Validators }    from '@angular/forms';
+import { FormsModule, FormControl, FormGroup, FormBuilder, FormArray, Validators, AbstractControl }    from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import { LogicielDto } from '../../entities/logiciel-dto';
@@ -20,6 +20,9 @@ export class BatchComponent implements OnInit {
 
   // Variable de stockage du fichier batch
   fichierBatch: Blob;
+  
+  // Variable de patteren de validation d'url de site web + getter
+  patternUrl: string = "(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?";
 
   // Variables pour la création de batch à partir du formulaire
   formulaireCreationBatch : FormGroup;
@@ -32,60 +35,66 @@ export class BatchComponent implements OnInit {
   messageErreur: string;
 
   // Constructeur
-  constructor(private batchService: BatchService,  private domSanitizer: DomSanitizer, private formBuilder: FormBuilder) { }
+  constructor(private batchService: BatchService,  private domSanitizer: DomSanitizer, private formBuilder: FormBuilder) {
+	  
+	  this.formulaireCreationBatch = this.formBuilder.group({
+		
+		  logiciels: this.formBuilder.array([
+			this.formBuilder.control(null)
+		  ]),
+		  urlSitewebs: this.formBuilder.array([
+			this.formBuilder.control(null, Validators.pattern(this.patternUrl))
+		  ])	  
+	});
+	
+  }
 
   ngOnInit(): void {
 
     // Initialisation du formulaire pour créer le batch
     this.creerFormulaireCreationBatch();
 
-    // Initialisation du formulaire pour récupérer le batch
-    this.creerFormulaireTelechargementBatch();
-
   }
 
   creerFormulaireCreationBatch() {
 
     // Variable de patteren de validation d'url de site web + getter
-    const patternUrl = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+    //const patternUrl = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
 
-    this.formulaireCreationBatch = this.formBuilder.group({
-      'idLogiciel1': [''],
-		  'nomLogiciel1': [''],
-      'repertoireLogiciel1': [''],
-      'idLogiciel2': [''],
-		  'nomLogiciel2': [''],
-      'repertoireLogiciel2': [''],
-      'idLogiciel3': [''],
-		  'nomLogiciel3': [''],
-      'repertoireLogiciel3': [''],
-      'idLogiciel4': [''],
-		  'nomLogiciel4': [''],
-      'repertoireLogiciel4': [''],
-      'idLogiciel5': [''],
-		  'nomLogiciel5': [''],
-      'repertoireLogiciel5': [''],
-      'idSiteweb1': [''],
-		  'urlSiteweb1': ['', Validators.pattern(patternUrl)],
-      'idSiteweb2': [''],
-		  'urlSiteweb2': ['', Validators.pattern(patternUrl)],
-      'idSiteweb3': [''],
-		  'urlSiteweb3': ['', Validators.pattern(patternUrl)],
-      'idSiteweb4': [''],
-		  'urlSiteweb4': ['', Validators.pattern(patternUrl)],
-      'idSiteweb5': [''],
-		  'urlSiteweb5': ['', Validators.pattern(patternUrl)]
-		});
+
+  }
+  
+  ajouterLogiciel(): void {
+    (this.formulaireCreationBatch.get("logiciels") as FormArray).push(
+      this.formBuilder.control(null)
+    );
   }
 
-  creerFormulaireTelechargementBatch() {
+  supprimerLogiciel(index) {
+    (this.formulaireCreationBatch.get("logiciels") as FormArray).removeAt(index);
+  }
+  
+  getLogicielsFormControls(): AbstractControl[] {
+    return (<FormArray> this.formulaireCreationBatch.get("logiciels")).controls;
+  }
+  
+  ajouterUrlSiteweb(): void {
+    (this.formulaireCreationBatch.get("urlSitewebs") as FormArray).push(
+      this.formBuilder.control(null, Validators.pattern(this.patternUrl))
+    );
+  }
 
-      this.formulaireTelechargementBatch = this.formBuilder.group({
-      'id': ['', Validators.required],
-		  'nom': ['', Validators.required],
-      'repertoire': ['', Validators.required]
-		});
-
+  supprimerUrlSiteweb(index) {
+    (this.formulaireCreationBatch.get("urlSitewebs") as FormArray).removeAt(index);
+  }
+  
+  getUrlSitewebsFormControls(): AbstractControl[] {
+    return (<FormArray> this.formulaireCreationBatch.get("urlSitewebs")).controls;
+  }
+  
+  getUrlSitewebFormControl(): AbstractControl[] {
+	console.log(this.formulaireCreationBatch.get("urlSiteweb").value);
+    return (<FormArray> this.formulaireCreationBatch.get("urlSiteweb")).value;
   }
 
   /* Méthode qui renvoie les controls du formlaire de création de batch:
@@ -130,74 +139,36 @@ export class BatchComponent implements OnInit {
     var sitewebDtos: SitewebDto[] = new Array();
     var logicielsEtSitesDto = new LogicielsEtSitesDto();
 
-    if(formulaireCreationDto.nomLogiciel1 !="") {
-      var logicielDto1: LogicielDto = new LogicielDto();
-      logicielDto1.setValues(1, formulaireCreationDto.nomLogiciel1, formulaireCreationDto.nomLogiciel1);
-      logicielDtos.push(logicielDto1);
-    }
+	// Ajout des logiciels
+	
+	var logiciels: string = this.formulaireCreationBatch.value.logiciels ;	
 
-    if(formulaireCreationDto.nomLogiciel2 !="") {
-      var logicielDto2: LogicielDto = new LogicielDto();
-      logicielDto2.setValues(1, formulaireCreationDto.nomLogiciel2, formulaireCreationDto.nomLogiciel2);
-      logicielDtos.push(logicielDto2);
-    }
+	for (var logiciel of logiciels) {
+      console.log(logiciel);
+      var logicielDto: LogicielDto = new LogicielDto(); 
+      logicielDto.setValues(1, logiciel, logiciel)	;
+      logicielDtos.push(logicielDto);			
+	}
 
-    if(formulaireCreationDto.nomLogiciel3 !="") {
-      var logicielDto3: LogicielDto = new LogicielDto();
-      logicielDto3.setValues(1, formulaireCreationDto.nomLogiciel3, formulaireCreationDto.nomLogiciel3);
-      logicielDtos.push(logicielDto3);
-    }
+	logicielsEtSitesDto.logicielDtos = logicielDtos;
+	
+	// Ajout des sites webs
+	
+	var urlSitewebs: string = this.formulaireCreationBatch.value.urlSitewebs ;	
 
-    if(formulaireCreationDto.nomLogiciel4 !="") {
-      var logicielDto4: LogicielDto = new LogicielDto();
-      logicielDto4.setValues(1, formulaireCreationDto.nomLogiciel4, formulaireCreationDto.nomLogiciel4);
-      logicielDtos.push(logicielDto4);
-    }
-
-    if(formulaireCreationDto.nomLogiciel5 !="") {
-      var logicielDto5: LogicielDto = new LogicielDto();
-      logicielDto5.setValues(1, formulaireCreationDto.nomLogiciel5, formulaireCreationDto.nomLogiciel5);
-      logicielDtos.push(logicielDto5);
-    }
-
-    logicielsEtSitesDto.logicielDtos = logicielDtos;
-
-    if(formulaireCreationDto.urlSiteweb1 !="") {
-      var sitewebDto1: SitewebDto = new SitewebDto();
-      sitewebDto1.setValues(1, formulaireCreationDto.urlSiteweb1);
-      sitewebDtos.push(sitewebDto1);
-    }
-
-    if(formulaireCreationDto.urlSiteweb2 !="") {
-      var sitewebDto2: SitewebDto = new SitewebDto();
-      sitewebDto2.setValues(1, formulaireCreationDto.urlSiteweb2);
-      sitewebDtos.push(sitewebDto2);
-    }
-
-    if(formulaireCreationDto.urlSiteweb3 !="") {
-      var sitewebDto3: SitewebDto = new SitewebDto();
-      sitewebDto3.setValues(1, formulaireCreationDto.urlSiteweb3);
-      sitewebDtos.push(sitewebDto3);
-    }
-
-    if(formulaireCreationDto.urlSiteweb4 !="") {
-      var sitewebDto4: SitewebDto = new SitewebDto();
-      sitewebDto4.setValues(1, formulaireCreationDto.urlSiteweb4);
-      sitewebDtos.push(sitewebDto4);
-    }
-
-    if(formulaireCreationDto.urlSiteweb5!="") {
-      var sitewebDto5: SitewebDto = new SitewebDto();
-      sitewebDto5.setValues(1, formulaireCreationDto.urlSiteweb5);
-      sitewebDtos.push(sitewebDto5);
-    }
-
+	for (var urlSiteweb of urlSitewebs) {
+      console.log(urlSiteweb);
+      var sitewebDto: SitewebDto = new SitewebDto(); 
+      sitewebDto.setValues(1, urlSiteweb)	;
+      sitewebDtos.push(sitewebDto);			
+	}
     logicielsEtSitesDto.sitewebDtos = sitewebDtos;
 
-    formulaireCreationDto = new FormulaireCreationDto();
+   formulaireCreationDto = new FormulaireCreationDto();
 
     return logicielsEtSitesDto;
 
   }
+ 
 
-}
+ }
